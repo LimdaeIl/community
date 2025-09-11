7b6127aea99cc5207cce17ca4e6d574f88b91072
LimdaeIl <optional94@naver.com>
2025-09-11 20:50:14 +0900

    ✨️(#25): 이메일 서비스 로직 연결 구현

--- src/main/java/com/community/soap/user/application/UserUseCase.java
+++ src/main/java/com/community/soap/user/application/UserUseCase.java
@@ -1,11 +1,15 @@
 package com.community.soap.user.application;
 
 import com.community.soap.common.resolver.CurrentUserInfo;
+import com.community.soap.user.application.request.EmailVerificationCodeRequest;
+import com.community.soap.user.application.request.EmailVerifyCodeRequest;
 import com.community.soap.user.application.request.SignInRequest;
 import com.community.soap.user.application.request.SignUpRequest;
+import com.community.soap.user.application.response.EmailVerificationCodeResponse;
 import com.community.soap.user.application.response.MyPageResponse;
 import com.community.soap.user.application.response.SignInResponse;
 import com.community.soap.user.application.response.SignUpResponse;
+import jakarta.validation.Valid;
 
 public interface UserUseCase {
 
@@ -22,4 +26,8 @@ public interface UserUseCase {
     void logout(String authorizationHeader, String refreshToken, Long userIdFromCtx);
 
     SignInResponse refresh(String refreshToken);
+
+    EmailVerificationCodeResponse emailVerificationCode(EmailVerificationCodeRequest request);
+
+    void emailVerifyCode(EmailVerifyCodeRequest request);
 }
