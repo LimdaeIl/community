987f168a77ba059354a29b940b508fd13f0436c4
LimdaeIl <optional94@naver.com>
2025-09-11 15:02:19 +0900

    ✨️(#23): softDelete 로 명칭 수정 및 공백 제거

--- src/main/java/com/community/soap/user/application/exception/UserErrorCode.java
+++ src/main/java/com/community/soap/user/application/exception/UserErrorCode.java
@@ -15,7 +15,6 @@ public enum UserErrorCode implements ErrorCode {
     PASSWORD_INCORRECT(HttpStatus.BAD_REQUEST, "비밀번호: 비밀번호가 틀립니다."),
     PASSWORD_NULL(HttpStatus.BAD_REQUEST, "비밀번호: 비밀번호 입력은 필수입니다."),
 
-
     EMAIL_DUPLICATED(HttpStatus.CONFLICT, "이메일: 이미 사용 중인 이메일입니다."),
     EMAIL_INVALID(HttpStatus.BAD_REQUEST, "이메일: 이메일 형식이 올바르지 않습니다."),
     EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "이메일: 이메일을 찾을 수 없습니다.");
